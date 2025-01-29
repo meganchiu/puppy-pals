46c3c76a5771d52ef104b36f3ceecd8be4103b83
Megan Chiu <megan.chiu.2014@gmail.com>
2025-01-28 20:04:29 -0800

    map over data using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {puppyList} from './data.js'
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log("puppies => ", puppies);
 
   return (
     <>
@@ -16,7 +20,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-
+        { 
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
